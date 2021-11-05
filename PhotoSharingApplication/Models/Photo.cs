74a806d1c66f9ae002f1285cd9198f5e6b251d86
ro <rochoagusti@gmail.com>
2021-11-05 17:04:37 -0300

    first commit

---
+++ PhotoSharingApplication/Models/Photo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhotoSharingApplication.Models
+{
+    public class Photo
+    {
+        public int PhotoID { get; set; }
+
+        public string Title { get; set; }
+
+        [DisplayName("Picture")]
+        public byte[] PhotoFile { get; set; }
+
+        public string ImageMimeType { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Created Date")]
+        [DisplayFormat(DataFormatString = "{0:MM / dd / yy}")]
+        public DateTime CreatedDate{ get; set; }
+
+        public string UserName { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+}
+}
---
+++ PhotoSharingApplication/Models/PhotoSharingContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+namespace PhotoSharingApplication.Models
+{
+    public class PhotoSharingContext:DbContext
+    {
+        public PhotoSharingContext() : base("KeyDB") { }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
